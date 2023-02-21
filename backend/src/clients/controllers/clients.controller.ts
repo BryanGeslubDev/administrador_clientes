e274d39013d7fefac45fa456d745be298304fed8
BryanGeslubDev <93697263+BjArandaV@users.noreply.github.com>
2023-02-21 13:33:30 -0300

    refactor clients controller

--- backend/src/clients/controllers/clients.controller.ts
+++ backend/src/clients/controllers/clients.controller.ts
@@ -1,60 +1,45 @@
 import {
-  Body,
   Controller,
   Get,
-  Param,
   Post,
   Put,
   Delete,
-  Res,
+  Body,
+  Param,
 } from '@nestjs/common';
-import { api_error } from 'src/utils/errors/controllerError';
-import { Clients } from '../client.entity';
 import { ClientsService } from '../services/clients.service';
+import { Clients } from '../client.entity';
 import { ClientDto } from '../dto/create-client.dto';
 
-@Controller('api/clients')
+@Controller('/api/clients')
 export class ClientsController {
-  constructor(private clientsService: ClientsService) {}
+  constructor(private readonly clientsService: ClientsService) {}
 
   @Get()
-  async getAll(@Res() res) {
-    try {
-      const clients = await this.clientsService.findAll();
-      console.log('Devolvemos todo');
-      return res.json({
-        message: `Clientes encontrados: ${clients.length}`,
-        data: clients,
-      });
-    } catch (e) {
-      console.log('error', e);
-      res.status(500).json({
-        message: api_error,
-      });
-    }
+  async findAll(): Promise<Clients[]> {
+    return this.clientsService.findAll();
   }
 
   @Get(':id')
-  getOne(@Param('id') id: number) {
+  async findOne(@Param('id') id: string): Promise<Clients> {
     return this.clientsService.findOne(id);
   }
 
   @Post()
-  create(@Body() body: ClientDto) {
-    return this.clientsService.create(body);
+  async create(@Body() createClientDto: ClientDto): Promise<Clients> {
+    return this.clientsService.create(createClientDto);
   }
 
   @Put(':id')
-  async update(@Param('id') id: number, @Body() body: Clients) {
-    const updateClient = await this.clientsService.findOne(id);
-    console.log(body);
-
-    await updateClient.update(body);
-    return this.clientsService.update(id);
+  async update(
+    @Param('id') id: string,
+    @Body() updateClientDto: Clients,
+  ): Promise<Clients> {
+    return this.clientsService.update(id, updateClientDto);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: number) {
+  async delete(@Param('id') id: string): Promise<void> {
     return this.clientsService.delete(id);
   }
 }
